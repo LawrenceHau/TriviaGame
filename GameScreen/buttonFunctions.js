009a7400d47105db6f0fe4986f5f860a78fd7f72
LawChristopherHau <Lawrencechristopherhau@gmail.com>
2021-07-29 19:37:35 -0400

    File configuration

---
+++ GameScreen/buttonFunctions.js
@@ -0,0 +1,44 @@
+// Button Ids
+let answers = document.querySelector('answers');
+let submit = document.querySelector('submit');
+let hint = document.querySelector('hint');
+let newQuestion = document.querySelector('newQuestion');
+
+
+
+//Function for Answers
+getAnswers = (e) => {
+    if (e.preventDefault) {
+
+    }
+
+};
+
+answers.addEventListener('click', getAnswers);
+//Function for Submitting Answer
+clickSubmit = (e) => {
+    if (e.preventDefault){
+        
+    }
+
+};
+
+submit.addEventListener('click', clickSubmit);
+//Function for gettin hint
+getHint = (e) => {
+    if (e.preventDefault){
+        
+    }
+
+};
+
+hint.addEventListener('click', getHint);
+//Function for getting a new question
+getNewQuestion = (e) => {
+    if (e.preventDefault){
+        
+    }
+    
+};
+
+newQuestion.addEventListener('click', getNewQuestion);
