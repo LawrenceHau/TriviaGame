b461fbd2c9212ed218e1fa25015875d2a88dfaba
LawChristopherHau <Lawrencechristopherhau@gmail.com>
2021-07-29 20:50:59 -0400

    added modal to stat page

--- GameScreen/Game.js
+++ GameScreen/Game.js
@@ -1,18 +1,18 @@
-//Used to open and close modals
-const openBtn = document.getElementById('openModal');
-const modal = document.getElementById('modal');
-const close = document.getElementById('close');
-//Blocks modal display
-const openModal = () => {
-    modal.style.display = 'block';
-};
-//Modal displayed on click
-openBtn.addEventListener('click', openModal);
-//Tells modal to exit
-const closeModal = () => {
-    modal.style.display = 'none';
-};
-//Modal exits on click
-close.addEventListener('click', closeModal);
+// //Used to open and close modals
+// const openBtn = document.getElementById('openModal');
+// const modal = document.getElementById('modal');
+// const close = document.getElementById('close');
+// //Blocks modal display
+// const openModal = () => {
+//     modal.style.display = 'block';
+// };
+// //Modal displayed on click
+// openBtn.addEventListener('click', openModal);
+// //Tells modal to exit
+// const closeModal = () => {
+//     modal.style.display = 'none';
+// };
+// //Modal exits on click
+// close.addEventListener('click', closeModal);
 
-setTimeout(openModal, 2000);
+// setTimeout(openModal, 2000);
---
+++ GameScreen/Modals.js
@@ -0,0 +1,19 @@
+//Creates stat modal and function
+let myStatModal = document.getElementById("statModal");
+let myStatButton = document.getElementById("statButton");
+let span = document.getElementsByClassName("close")[0];
+console.log(span)
+
+myStatButton.onclick = function() {
+    myStatModal.style.display = "block";
+}
+
+span.onclick = function() {
+    myStatModal.style.display = "none";
+}
+
+window.onclick = function (e) {
+    if (e.target == statModal){
+        myStatModal.style.display = "none";
+    }
+}
