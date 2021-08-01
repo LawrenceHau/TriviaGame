073b475bc5dbb5b7839514855e18bc277aa55a7b
LawChristopherHau <Lawrencechristopherhau@gmail.com>
2021-07-31 21:38:21 -0400

    Got sick and wanted to focus on functionality with limited time, project basically restarted

--- GameScreen/Game.js
+++ GameScreen/Game.js
@@ -1,18 +1,61 @@
-// //Used to open and close modals
-// const openBtn = document.getElementById('openModal');
-// const modal = document.getElementById('modal');
-// const close = document.getElementById('close');
-// //Blocks modal display
-// const openModal = () => {
-//     modal.style.display = 'block';
-// };
-// //Modal displayed on click
-// openBtn.addEventListener('click', openModal);
-// //Tells modal to exit
-// const closeModal = () => {
-//     modal.style.display = 'none';
-// };
-// //Modal exits on click
-// close.addEventListener('click', closeModal);
+// Gets the question 
+let question = document.querySelector('#question')
+let scoreStart = document.querySelector('#scoreStart')
+let answer = null
+// Gets yes and no buttons
+const yes = document.querySelector('#yes')
+const no = document.querySelector('#no')
 
-// setTimeout(openModal, 2000);
+// Gives yes and no true and false values.
+yes.value = true
+no.value = false
+//  variables for current question & availiable questions
+let currentQuestion = {}
+let availiableQuestions = []
+let totalQuestions = 0
+
+// Array for filling in question text
+questionText = [
+    {
+        question: 'fill',
+        answer: true
+    },
+    {
+        question: 'fill',
+        answer: false
+    },
+    {
+        question: 'fill',
+        answer: true
+    },
+    {
+        question: 'fill',
+        answer: false
+    },
+
+]
+
+//Testing yes and no values
+function testYes(){
+    console.log(yes.value)
+}
+yes.addEventListener('click', testYes)
+
+function testNo(){
+    console.log(no.value)
+}
+no.addEventListener('click', testNo)
+
+
+
+
+
+//  starts the game if score start is 0, and iterates over the availaible questions starts the get new question function
+startGame = () => {
+    getNewQuestion()
+}
+// Function for getting new question
+getNewQuestion = () => {
+    question.innerText = questionText;
+
+    }
